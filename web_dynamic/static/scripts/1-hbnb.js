35e5b951c00d36830e2edd669dc5ec665c57f257
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-10-07 09:15:15 +0000

    Update 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
-    $('input[type="checkbox"]').css("margin-right", "10px");
-    let amenityIdList = []
+    $("amenities").css();
+    $('input[type="checkbox"]').css("margin-right", "5px");
+    $(".filters h4").css({
+	"margin-left": "15%",
+	"margin-top": "0",
+	"height": "15px",
+	"font-weight": "400",
+	"font-size": "14px",
+	"white-space": "nowrap",
+	"overflow": "hidden",
+	"text-overflow": "ellipsis"
+    });
+    let amenityDict = {}
     $('input[type="checkbox"]').on("change", function() {
 	let amenityId = $(this).data("id");
+	let amenityName = $(this).data("name");
 	if ($(this).is(":checked")) {
-	    amenityIdList.push(amenityId);
+	    amenityDict[amenityName] = amenityId;
 	} else {
-	    let index = amenityIdList.indexOf(amenityId);
-	    if (index !== -1) {
-		amenityIdList.splice(index, 1);
-	    }
+	    delete amenityDict[amenityName];
 	}
-	let amenitiesText = amenityIdList.join(", ");
-	$("div h4").text(amenitiesText);
+	let amenitiesText = Object.keys(amenityDict).join(", ");
+	$("div.amenities h4").text(amenitiesText);
     });
 });
